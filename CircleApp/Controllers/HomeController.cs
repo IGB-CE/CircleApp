5ce8d6ae8761f14db883b1486067c899255a50fa
Igli <iglibraho77@gmail.com>
2024-12-15 17:59:44 +0100

    changes

--- CircleApp/Controllers/HomeController.cs
+++ CircleApp/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
+using CircleApp.Data;
+using CircleApp.Data.Models;
+using CircleApp.ViewModels.Home;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace CircleApp.Controllers
@@ -6,20 +10,66 @@ namespace CircleApp.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext _appDbContext;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, AppDbContext context)
         {
             _logger = logger;
+            _appDbContext = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var allPosts = await _appDbContext.Posts
+                .Include(n => n.User)
+                .OrderByDescending(n => n.Id)
+                .ToListAsync();
+            return View(allPosts);
         }
 
-        public IActionResult Privacy()
+        [HttpPost]
+        public async Task<IActionResult> CreatePost(PostVM post)
         {
-            return View();
+            //Get the logged in user
+            int loggedInUser = 1;
+
+            //Create a new post
+            var newPost = new Post
+            {
+                Content = post.Content,
+                DateCreated = DateTime.UtcNow,
+                DateUpdated = DateTime.UtcNow,
+                ImageUrl = "",
+                NrOfReports = 0,
+                UserId = loggedInUser
+            };
+
+            //Check and save the image
+            if(post.Image != null && post.Image.Length > 0)
+            {
+                string rootFolderPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot");
+                if (post.Image.ContentType.Contains("image"))
+                {
+                    string rootFolderPathImages = Path.Combine(rootFolderPath, "images/uploaded");
+                    Directory.CreateDirectory(rootFolderPathImages);
+
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(post.Image.FileName);
+                    string filePath = Path.Combine(rootFolderPathImages, fileName);
+
+                    using(var stream = new FileStream(filePath,FileMode.Create))
+                        await post.Image.CopyToAsync(stream);
+
+                    //Set the Url to the new post object
+                    newPost.ImageUrl = "/images/uploaded/" + fileName;
+                }
+            }
+
+            //Add the post to database
+            await _appDbContext.Posts.AddAsync(newPost);
+            await _appDbContext.SaveChangesAsync();
+
+            //Redirect to the index page
+            return RedirectToAction("Index");
         }
     }
 }
